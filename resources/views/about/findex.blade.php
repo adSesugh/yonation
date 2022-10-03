2fe67030f2d11ee98adacfbd80aaf336f9c0c157
Agbadu Sesugh <sesughagbadu@yahoo.com>
2022-10-03 19:06:33 +0100

    footer adjustment

--- resources/views/about/findex.blade.php
+++ resources/views/about/findex.blade.php
@@ -4,4 +4,9 @@
 
 @section('content')
     @include('layouts.partials.breadcrumb')
+    <div class="row mt-4 mx-4">
+        <p>
+            The Yoruba people are a West African ethnic group that mainly inhabit parts of Nigeria, Benin and Togo. The areas of these countries primarily inhabited by Yorubas are often collectively referred to as Yorubaland.
+        </p>
+    </div>
 @stop
