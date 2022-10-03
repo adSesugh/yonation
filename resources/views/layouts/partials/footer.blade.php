f38101caa2ddce3cea5b6485e1563c61daf0b939
Agbadu Sesugh <sesughagbadu@yahoo.com>
2022-10-03 18:56:58 +0100

    footer adjustment

--- resources/views/layouts/partials/footer.blade.php
+++ resources/views/layouts/partials/footer.blade.php
@@ -82,11 +82,11 @@
                         @forelse (getBlogs() as $item)
                             <li class="footer-item">
                                 <a href="#" class="footer-link">
-                                    <div class="row">
+                                    <div class="row no-gutters">
                                         <div class="col-lg-3">
                                             <img src="{{ $item->media[0]->original_url }}" style="height: 80px;width: 80px;" alt="{{ $item->title }}">
                                         </div>
-                                        <div class="col-lg-9">
+                                        <div class="col-lg-9 mx-1">
                                             <h5>{{ $item->title }}</h5>
                                             <span>{{ $item->category->name }}</span>
                                             <span>{{ Str::limit($item->description, 65) }}</span>
