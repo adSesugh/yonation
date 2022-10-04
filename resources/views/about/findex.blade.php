c3585034a79ad5a6837339e5d7bb070c070b2150
Agbadu Sesugh <sesughagbadu@yahoo.com>
2022-10-04 10:45:42 +0100

    Update about us page

--- resources/views/about/findex.blade.php
+++ resources/views/about/findex.blade.php
@@ -5,8 +5,19 @@
 @section('content')
     @include('layouts.partials.breadcrumb')
     <div class="row mt-4 mx-4">
-        <p>
-            The Yoruba people are a West African ethnic group that mainly inhabit parts of Nigeria, Benin and Togo. The areas of these countries primarily inhabited by Yorubas are often collectively referred to as Yorubaland.
-        </p>
+        <div class="col-lg-2"></div>
+        <div class="col-lg-8">
+            <div class="row mb-4">
+                <iframe width="420" height="450" src="https://www.youtube.com/embed/fRy92OJCtcY" title="The Yoruba from Prehistory to the Present" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+            </div>
+            <p>
+                The Yoruba are among the most storied groups in Africa. Their ancestral homeland cuts across present-day southwest Nigeria, Benin Republic and Togo in West Africa. They number between 40 and 60 million. Their dynamic culture, philosophy, arts, language, sociology and history have attracted numerous studies.
+            </p>
+            <p>What has been missing in this rich literature is a deep history that benefits from a diverse range of disciplines and sources. Scholars have long recognised the value of combining different methods and sources, beyond documentary and oral traditions, to study pre-colonial African history.</p>
+            <p>Between the 12th and 14th centuries, Ile-Ife was the centre of the Yoruba world. It was an emporium and holy city. Its economy was based on a novel technology of glass manufacture mainly devoted to making beads, the primary currency of power, authority and wealth in the region.</p>
+            <p>Ile-Ife remains the only place in sub-Saharan Africa known as an industrial centre for primary glass production.</p>
+            <p>Ile-Ife used its technological and economic advantages to restructure the ideology of divine kingship. It also used this advantage to standardise the Yoruba religious system (Orisa pantheon) and make itself (literally) the beginning and end of time. It brought vast territories, as far as the River Niger and the Atlantic coast, under its political control and cultural influence. These included even non-Yoruba-speaking peoples.</p>
+        </div>
+        <div class="col-lg-2"></div>
     </div>
 @stop
