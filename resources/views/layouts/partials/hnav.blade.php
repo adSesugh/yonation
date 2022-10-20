352700ec581b95bd5e192cc184662bb82a8f2a87
Agbadu Sesugh <sesughagbadu@yahoo.com>
2022-10-20 12:13:54 +0100

    logo editing

--- resources/views/layouts/partials/hnav.blade.php
+++ resources/views/layouts/partials/hnav.blade.php
@@ -2,8 +2,8 @@
 <nav id="navbar" class="navbar navbar-expand-lg fixed-top sticky">
     <div class="container">
         <a class="navbar-brand" href="{{ route('index') }}">
-            <img src="{{ asset('images/ynlogo.png') }}" alt="logo" height="80" class="logo-light" />
-            <img src="{{ asset('images/ynlogo.png') }}" alt="logo" height="80" class="logo-dark" />
+            <img src="{{ asset('images/ynlogo.png') }}" alt="logo" height="60" class="logo-light" />
+            <img src="{{ asset('images/ynlogo.png') }}" alt="logo" height="60" class="logo-dark" />
         </a>
         <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarCollapse"
             aria-controls="navbarCollapse" aria-expanded="false" aria-label="Toggle navigation">
--- resources/views/layouts/partials/sidebar.blade.php
+++ resources/views/layouts/partials/sidebar.blade.php
@@ -4,7 +4,7 @@
     <!-- Sidebar - Brand -->
     <a class="sidebar-brand d-flex align-items-center justify-content-center" href="index.html">
         <div class="sidebar-brand-text mx-3">
-            <img src="{{ asset('images/ynlogo.png') }}" height="80" />
+            <img src="{{ asset('images/ynlogo.png') }}" height="60" />
         </div>
     </a>
 
