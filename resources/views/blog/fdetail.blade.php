82eb5ab30477fecce487714974205eb304acacd6
Agbadu Sesugh <sesughagbadu@yahoo.com>
2022-11-04 08:36:07 +0100

    translate logic

--- resources/views/blog/fdetail.blade.php
+++ resources/views/blog/fdetail.blade.php
@@ -6,7 +6,7 @@
     @include('layouts.partials.breadcrumb')
     <div class="row my-3">
         <div class="col-lg-8">
-            <img src="{{ $detail->media[0]->getOrginalUrl() }}" alt="{{ Str::lower($detail->title) }}">
+            <img src="{{ $detail->media[0]->original_url }}" alt="{{ Str::lower($detail->title) }}">
         </div>
         <div class="col-lg-4">
 
