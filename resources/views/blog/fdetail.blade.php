ce6d588b166937b94c5343241aa2ee8b20192bf7
Agbadu Sesugh <sesughagbadu@yahoo.com>
2022-11-04 08:39:38 +0100

    blog detail

--- resources/views/blog/fdetail.blade.php
+++ resources/views/blog/fdetail.blade.php
@@ -7,6 +7,8 @@
     <div class="row my-3 mx-4">
         <div class="col-lg-8">
             <img width="100%" src="{{ $detail->media[0]->original_url }}" alt="{{ Str::lower($detail->title) }}">
+            <p></p>
+            <p>{{ $detail->description }}</p>
         </div>
         <div class="col-lg-4">
 
