c6bc9245bd4bfc34f8530165c2b8025c545712f9
Agbadu Sesugh <sesughagbadu@yahoo.com>
2022-11-04 08:42:48 +0100

    blog detail

--- resources/views/blog/fdetail.blade.php
+++ resources/views/blog/fdetail.blade.php
@@ -6,8 +6,18 @@
     @include('layouts.partials.breadcrumb')
     <div class="row my-3 mx-4">
         <div class="col-lg-8">
+            <div class="row">
+                <h5>{{ $detail->title }}</h5>
+            </div>
             <img width="100%" src="{{ $detail->media[0]->original_url }}" alt="{{ Str::lower($detail->title) }}">
-            <p></p>
+            <div class="row my-2">
+                <div class="col-lg-6">
+                    {{ $detail->category->name }}
+                </div>
+                <div class="col-lg-6">
+                    {{ \Carbon\Carbon::parse($item->created_at)->format('F j, Y') }}
+                </div>
+            </div>
             <p>{{ $detail->description }}</p>
         </div>
         <div class="col-lg-4">
