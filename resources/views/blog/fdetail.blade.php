3f9653c594973f5bfc7f108ed847c94ce2b56a5b
Agbadu Sesugh <sesughagbadu@yahoo.com>
2022-11-05 11:21:30 +0100

    blog detail

--- resources/views/blog/fdetail.blade.php
+++ resources/views/blog/fdetail.blade.php
@@ -31,8 +31,10 @@
                                     <img src="{{ $blog->media[0]->original_url }}" alt="{{ $blog->title }}" height="60px" width="60px">
                                 </div>
                                 <div class="col-lg-8 pl-4">
-                                    <h6>{{ $blog->title }}</h6>
-                                    <p>{{ Str::limit($blog->description, 20) }} <a style="text-decoration: black" href="{{ route('blog.detail', $blog->slug) }}">Read me...</a></p>
+                                    <div class="row">
+                                        <h6>{{ $blog->title }}</h6>
+                                        <p>{{ Str::limit($blog->description, 20) }} <a style="text-decoration: black" href="{{ route('blog.detail', $blog->slug) }}">Read me...</a></p>
+                                    </div>
                                 </div>
                             </div>
                         </div>
