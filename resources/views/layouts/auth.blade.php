ea6e5ccf066798f1ffdad477198f1dcc6ba16d9f
Sesugh Agbadu <sesughagbadu@yahoo.com>
2022-11-09 13:07:21 +0100

    add job function
    	modified:   resources/views/layouts/auth.blade.php
    	modified:   resources/views/layouts/guest.blade.php

--- resources/views/layouts/auth.blade.php
+++ resources/views/layouts/auth.blade.php
@@ -6,7 +6,6 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
     <meta name="description" content="Yoruba Nation">
-    <meta name="author" content="Agbadu Sesugh">
 
     <title>@yield('title') | {{ config('app.name', 'Yoruba Nation') }}</title>
     
--- resources/views/layouts/guest.blade.php
+++ resources/views/layouts/guest.blade.php
@@ -6,7 +6,6 @@
     <meta charset="utf-8" />
     <title>@yield('title') | {{ getSetting('app_name') }}</title>
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-    <meta content="KeennessSolutions" name="Agbadu Sesugh" />
 
     <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png">
     <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png">
